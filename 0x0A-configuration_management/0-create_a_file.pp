f41bf426ab2f7e1fa8e5fc3de4a4f17de7d8aea4
surafel27 <surafelfekadu399@gmail.com>
2023-03-24 05:27:00 -0700

    Using Puppet, create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/school':
+# Creating a file
+file { '/tmp/school': # the path of the new file
     ensure  => 'present',
     replace => 'no',
-    content => 'I love Puppet',
-    mode    => '0744',
+    content => 'I love Puppet', # the content of the file
+    mode    => '0744', # permission to the file
     owner   => 'www-data',
     group   => 'www-data',
 }
