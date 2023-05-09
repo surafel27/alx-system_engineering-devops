6ae2a3f2d76a2d8f119fd479c79dc19f6c586e98
surafel27 <surafefekadu399@gmail.com.com>
2023-05-09 12:05:29 -0700

    returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,20 +6,21 @@ articles for a given subreddit"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """Set the URL for the subreddit's hot posts API"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "My User Agent 1.0"}
-    if after:
-        url += "?after={}".format(after)
+    url += "?after={}?count={}?limit=100".format(after, count)
+    headers = {"User-Agent": "python:subreddit_hot_posts:v1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
         return None
     data = response.json()
     posts = data["data"]["children"]
     titles = [post["data"]["title"] for post in posts]
+    hot_list += titles
     after = data["data"]["after"]
+    count += data["data"]["dist"]
     if after:
-        recurse(subreddit, hot_list, after)
-
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
