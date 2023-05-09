8739b790e40f9a5120ddf1832102a6c43738533e
surafel27 <surafefekadu399@gmail.com.com>
2023-05-09 11:23:19 -0700

    prints the titles of the first 10 hot posts listed

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries the Reddit API and prints the titles
+   of the first 10 hot posts listed"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """return top_ten hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "python:subreddit_hot_posts:v1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()
+    posts = data["data"]["children"][:10]
+    titles = [post["data"]["title"] for post in posts]
+    for title in titles:
+        print(title)
